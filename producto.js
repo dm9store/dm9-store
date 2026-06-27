6591df7954f0f5f561ac108d89dcd07a9149687b
Diego <dm9store999@gmail.com>
2026-06-27 14:15:59 -0300

    Corregido búsqueda por id

--- producto.js
+++ producto.js
@@ -1,5 +1,6 @@
 const params = new URLSearchParams(window.location.search);
-const id = params.get("id");
+const id = Number(params.get("id"));
+
 
 const producto = productos.find(p => p.id === id);
 

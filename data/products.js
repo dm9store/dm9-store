a7143b79df86d1a2c6ffb20bc8ffc68c37a09b96
Diego <dm9store999@gmail.com>
2026-08-01 15:48:03 -0300

    Reajuste de JPG a JPEG

--- data/products.js
+++ data/products.js
@@ -470,7 +470,7 @@ const productos = [
     categoria: "Hogar",
     precio: 120000,
     moneda: "Gs",
-    imagen: "images/products/tomapresion-ecopower-ep2746.jpg",
+    imagen: "images/products/tomapresion-ecopower-ep2746.jpeg",
     descripcion: "Monitor electrónico de presión arterial de brazo Ecopower EP-2746. Pantalla digital de fácil lectura, medición automática, memoria para múltiples registros y alta precisión para el control diario de la presión arterial."
 },
 
@@ -481,7 +481,7 @@ const productos = [
     categoria: "Hogar",
     precio: 150000,
     moneda: "Gs",
-    imagen: "images/products/masajeador-cuello-luo-lu4987.jpg",
+    imagen: "images/products/masajeador-cuello-luo-lu4987.jpeg",
     descripcion: "Masajeador eléctrico LUO diseñado para cuello, hombros, espalda, cintura y piernas. Cuenta con varios niveles de intensidad para aliviar la tensión muscular y brindar una experiencia relajante en cualquier momento."
 },
 
@@ -492,7 +492,7 @@ const productos = [
     categoria: "Hogar",
     precio: 80000,
     moneda: "Gs",
-    imagen: "images/products/pistola-masajeadora-luo-lu4983.jpg",
+    imagen: "images/products/pistola-masajeadora-luo-lu4983.jpeg",
     descripcion: "Mini pistola masajeadora inalámbrica LUO ideal para relajar músculos, aliviar la fatiga y acelerar la recuperación después del ejercicio. Compacta, potente y fácil de transportar."
 },
 ];
